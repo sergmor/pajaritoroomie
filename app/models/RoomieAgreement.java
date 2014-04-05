1546463c62d89349a7f1407c042b066a532cee74
Daniel <sdm2162@columbia.edu>
2014-04-05 17:24:08 -0400

    Model1

--- app/models/RoomieAgreement.java
+++ app/models/RoomieAgreement.java
@@ -1,7 +1,18 @@
 package models;
 
+import java.util.List;
+
 import play.db.ebean.Model;
 
 public class RoomieAgreement extends Model {
+	
+	public List<Owner> roomies;
 
+	public RoomieAgreement(List<Owner> roomies) {
+		super();
+		this.roomies = roomies;
+	}
+	
+	
+	
 }
