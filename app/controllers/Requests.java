e037811b2466e7384a81becd4593ee979ea54da1
Daniel <sdm2162@columbia.edu>
2014-04-06 01:20:06 -0400

    Req to Fix

--- app/controllers/Requests.java
+++ app/controllers/Requests.java
@@ -2,17 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import play.libs.Json;
 import play.mvc.BodyParser;
@@ -26,6 +28,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+
 import javax.imageio.ImageIO;
 
 public class Requests extends Controller{
@@ -34,10 +37,10 @@ public class Requests extends Controller{
 	public static Result newRequest() {
 		JsonNode jsonNode = Controller.request().body().asJson();
 		String email = jsonNode.findPath("email").asText();
-		byte[] fileBytes = (byte[]) jsonNode.findPath("file");
+		//byte[] fileBytes = jsonNode.findPath("file");
 
-		InputStream in = new ByteArrayInputStream(fileBytes);
-		BufferedImage bImageFromConvert = ImageIO.read(in);
+		//InputStream in = new ByteArrayInputStream(fileBytes);
+		//BufferedImage bImageFromConvert = ImageIO.read(in);
 
 		String timestamp = jsonNode.findPath("timestamp").asText();
 				
